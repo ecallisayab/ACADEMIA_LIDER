9c7213d1d10300cc0b2904677af4781363a6c8f2
Cecilio Poma <poma6185@gmail.com>
2022-05-20 05:17:43 -0400

    login,catalogos y usuarios

---
+++ AcademiaLider/CapaAccesoDatos/Accesslogin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+using AcademiaLider.Core;
+namespace AcademiaLider.CapaAccesoDatos
+{
+    class Accesslogin
+    {
+        private ConexionBaseDatos conn;
+        public Accesslogin()
+        {
+            conn = new ConexionBaseDatos();
+        }
+        public Boolean login(String user, String password)
+        {
+            Boolean acceso = false;
+            try
+            {
+
+                String sql = "select * from usuarios where usuario =@us and CONVERT(VARCHAR(MAX), DECRYPTBYPASSPHRASE('password',contrasena))= @con";
+                SqlCommand comando = new SqlCommand(sql, conn.Conectar());
+                comando.Parameters.AddWithValue("@us", user);
+                comando.Parameters.AddWithValue("@con", password);
+                SqlDataReader lector = comando.ExecuteReader();
+                acceso = lector.HasRows;
+                conn.Cerrar();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Se produjo un error desconocido");
+            }
+            return acceso;
+        }
+    }
+}
